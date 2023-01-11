ccf28622b56ab31fb22fc8b3fd48b07c259c054b
Lukasz Wisniewski <wsa3@vp.pl>
2023-01-11 12:10:39 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Witam cię u mnie po raz kolejny")
+
+let button = document.querySelector(".button__js-removeImage");
+let image = document.querySelector(".image");
+
+button.addEventListener("click", () => {
+    image.classList.toggle("noFoto");
+    button.innerText =
+        image.classList.contains("noFoto")
+            ? "pokaż fotkę" : "usuń fotkę";
+
+});
+let body = document.querySelector(".body");
+let changeBackgroundButton = document.querySelector(".button__js-changeBackground");
+let nextColorName = document.querySelector(".nextColorName");
+
+changeBackgroundButton.addEventListener("click", () => {
+    body.classList.toggle("whiteBackground");
+    nextColorName.innerText =
+        body.classList.contains("whiteBackground")
+            ? "ciemne" : "jasne";
+
+});
