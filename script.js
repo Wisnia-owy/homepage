b96f7b75e8c6b9f0046634cd17d7f1b5124007b2
Lukasz Wisniewski <wsa3@vp.pl>
2023-02-11 15:24:22 +0000

    Changed JS for functions

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-console.log("Witam cię u mnie po raz kolejny.Ten kod jest juz w repozytoriom git drugi raz.")
+{
+    const welcome = () => {
+    console.log("Witam cię u mnie po raz kolejny.Ten kod jest juz w repozytoriom git drugi raz.")
+
+}
+
+const toggleBackground = () => {
+    const body = document.querySelector(".body");
+    const nextColorName = document.querySelector(".nextColorName");
+    body.classList.toggle("whiteBackground");
+    nextColorName.innerText =
+        body.classList.contains("whiteBackground")
+            ? "ciemne" : "jasne";
+
+};
+
+const init = () => {
+    
+const changeBackgroundButton = document.querySelector(".button__js-changeBackground");
+
+
+changeBackgroundButton.addEventListener("click", toggleBackground );
+welcome();
+};
+
+init();
+
 
 let button = document.querySelector(".button__js-removeImage");
 let image = document.querySelector(".image");
@@ -10,14 +36,6 @@ button.addEventListener("click", () => {
             ? "pokaż fotkę" : "usuń fotkę";
 
 });
-let body = document.querySelector(".body");
-let changeBackgroundButton = document.querySelector(".button__js-changeBackground");
-let nextColorName = document.querySelector(".nextColorName");
+ 
 
-changeBackgroundButton.addEventListener("click", () => {
-    body.classList.toggle("whiteBackground");
-    nextColorName.innerText =
-        body.classList.contains("whiteBackground")
-            ? "ciemne" : "jasne";
-
-});
+ }
